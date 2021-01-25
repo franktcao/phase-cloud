affbe75268bb3969c7443a32a6ce192fe1a7a1bc
Frank Cao <franktcao@gmail.com>
2021-01-24 19:19:50 -0500

    [PCLOUD-5] Black reformat; optimize imports; add docstring; update requirements.txt; rename main file that calls app

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,31 +1,36 @@
 import pickle
 from flask import (
-    Flask, redirect, render_template, request, session, url_for,
+    Flask,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
 )
 
 from .model import predict
 
-'''
+"""
 Initiate a new flaskr app
 1. Input some random secret key to be used by the application 
-'''
+"""
 app = Flask(__name__)
 app.config.from_mapping(
-    SECRET_KEY='\xe0\xcd\xac#\x06\xd9\xe4\x00\xa5\xf2\x88\xc3\xef$\xa5\x05n\x97\xd8\x1269i\xd3'
+    SECRET_KEY="\xe0\xcd\xac#\x06\xd9\xe4\x00\xa5\xf2\x88\xc3\xef$\xa5\x05n\x97\xd8\x1269i\xd3"
 )
 
 
-'''
+"""
 Load the machine learning libraries 
 1. Logistic regression model is used to predict the sentiment on the newly computed matrix
-'''
+"""
 
 # Load the machine learning model
-with open('./flaskr/static/LogisticRegression.pickle', 'rb') as input_file:
+with open("./flaskr/static/LogisticRegression.pickle", "rb") as input_file:
     model = pickle.load(input_file)
 
 
-'''
+"""
 Home Page
 1. It will take both GET and POST requests 
 2. For GET request, base.html (homepage) will be rendered without any results shown
@@ -33,19 +38,21 @@ Home Page
     a) Session will then be cleared (to remove anything belonged to previous session) and 'message' will be passed into the session 
     so that it can be reused throughout the session
     b) The page will then be redirected to /result page
-'''
-@app.route('/', methods=('GET', 'POST'))
+"""
+
+
+@app.route("/", methods=("GET", "POST"))
 def index():
-    if request.method == 'POST':
-        message = request.form['message']
+    if request.method == "POST":
+        message = request.form["message"]
         if message is not None:
             session.clear()
-            session['message'] = message
-            return redirect(url_for('result'))
+            session["message"] = message
+            return redirect(url_for("result"))
     return render_template("base.html")
 
 
-'''
+"""
 Result Page
 1. It will take both GET and POST requests 
 2. For GET request, 'message' will be obtained from the session, remember the 'message' is from the Home page! 
@@ -55,17 +62,21 @@ Result Page
     a) Session will then be cleared (to remove anything belonged to previous session) and 'message' will be passed into the session 
     so that it can be reused throughout the session
     c) The page will then be redirected to /result page
-'''
-@app.route('/result', methods=('GET', 'POST'))
+"""
+
+
+@app.route("/result", methods=("GET", "POST"))
 def result():
-    message = session.get('message')
+    message = session.get("message")
     df_pred = predict(model=model, text=message)
-    sentiment = df_pred.head(1)['sentiment'].values[0]
-    score = df_pred.head(1)['score'].values[0]
-    if request.method == 'POST':
-        message = request.form['message']
+    sentiment = df_pred.head(1)["sentiment"].values[0]
+    score = df_pred.head(1)["score"].values[0]
+    if request.method == "POST":
+        message = request.form["message"]
         if message is not None:
             session.clear()
-            session['message'] = message
-            return redirect(url_for('result'))
-    return render_template("result.html", message=message, sentiment=sentiment, score=score)
+            session["message"] = message
+            return redirect(url_for("result"))
+    return render_template(
+        "result.html", message=message, sentiment=sentiment, score=score
+    )
